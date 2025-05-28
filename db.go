637d701fc128fc6a1d88f826b7f8c573013ef1f0
Roman Khimov <roman@nspcc.ru>
2025-05-28 18:52:34 +0300

    dirt

--- db.go
+++ db.go
@@ -40,7 +40,7 @@ type DB struct {
 	// the first word in an allocated struct can be relied upon to be 64-bit
 	// aligned. Refer to https://pkg.go.dev/sync/atomic#pkg-note-BUG. Also
 	// refer to discussion in https://github.com/etcd-io/bbolt/issues/577.
-	stats Stats
+	stats *Stats
 
 	// When enabled, the database will perform a Check() after every commit.
 	// A panic is issued if the database is in an inconsistent state. This
@@ -132,7 +132,6 @@ type DB struct {
 	pageSize int
 	opened   bool
 	rwtx     *Tx
-	txs      []*Tx
 
 	freelist     fl.Interface
 	freelistLoad sync.Once
@@ -143,7 +142,7 @@ type DB struct {
 	batch   *batch
 
 	rwlock   sync.Mutex   // Allows only one writer at a time.
-	metalock sync.Mutex   // Protects meta page access.
+	metalock sync.RWMutex // Protects meta page access.
 	mmaplock sync.RWMutex // Protects mmap access during remapping.
 	statlock sync.RWMutex // Protects stats access.
 
@@ -178,6 +177,7 @@ func (db *DB) String() string {
 func Open(path string, mode os.FileMode, options *Options) (db *DB, err error) {
 	db = &DB{
 		opened: true,
+		//		stats:  &Stats{},
 	}
 
 	// Set default options if no options are provided.
@@ -424,7 +424,9 @@ func (db *DB) loadFreelist() {
 			// Read free list from freelist page.
 			db.freelist.Read(db.page(db.meta().Freelist()))
 		}
-		db.stats.FreePageN = db.freelist.FreeCount()
+		if db.stats != nil {
+			db.stats.FreePageN = db.freelist.FreeCount()
+		}
 	})
 }
 
@@ -769,7 +771,7 @@ func (db *DB) beginTx() (*Tx, error) {
 	// Lock the meta pages while we initialize the transaction. We obtain
 	// the meta lock before the mmap lock because that's the order that the
 	// write transaction will obtain them.
-	db.metalock.Lock()
+	db.metalock.RLock()
 
 	// Obtain a read-only lock on the mmap. When the mmap is remapped it will
 	// obtain a write lock so all transactions must finish before it can be
@@ -779,14 +781,14 @@ func (db *DB) beginTx() (*Tx, error) {
 	// Exit if the database is not open yet.
 	if !db.opened {
 		db.mmaplock.RUnlock()
-		db.metalock.Unlock()
+		db.metalock.RUnlock()
 		return nil, berrors.ErrDatabaseNotOpen
 	}
 
 	// Exit if the database is not correctly mapped.
 	if db.data == nil {
 		db.mmaplock.RUnlock()
-		db.metalock.Unlock()
+		db.metalock.RUnlock()
 		return nil, berrors.ErrInvalidMapping
 	}
 
@@ -794,21 +796,20 @@ func (db *DB) beginTx() (*Tx, error) {
 	t := &Tx{}
 	t.init(db)
 
-	// Keep track of transaction until it closes.
-	db.txs = append(db.txs, t)
-	n := len(db.txs)
+	// Unlock the meta pages.
+	db.metalock.RUnlock()
+
 	if db.freelist != nil {
 		db.freelist.AddReadonlyTXID(t.meta.Txid())
 	}
 
-	// Unlock the meta pages.
-	db.metalock.Unlock()
-
-	// Update the transaction stats.
-	db.statlock.Lock()
-	db.stats.TxN++
-	db.stats.OpenTxN = n
-	db.statlock.Unlock()
+	if db.stats != nil {
+		// Update the transaction stats.
+		db.statlock.Lock()
+		db.stats.TxN++
+		db.stats.OpenTxN++
+		db.statlock.Unlock()
+	}
 
 	return t, nil
 }
@@ -844,7 +845,7 @@ func (db *DB) beginRWTx() (*Tx, error) {
 	t := &Tx{writable: true}
 	t.init(db)
 	db.rwtx = t
-	db.freelist.ReleasePendingPages()
+	db.freelist.ReleasePendingPages(t.meta.Txid())
 	return t, nil
 }
 
@@ -853,32 +854,18 @@ func (db *DB) removeTx(tx *Tx) {
 	// Release the read lock on the mmap.
 	db.mmaplock.RUnlock()
 
-	// Use the meta lock to restrict access to the DB object.
-	db.metalock.Lock()
-
 	// Remove the transaction.
-	for i, t := range db.txs {
-		if t == tx {
-			last := len(db.txs) - 1
-			db.txs[i] = db.txs[last]
-			db.txs[last] = nil
-			db.txs = db.txs[:last]
-			break
-		}
-	}
-	n := len(db.txs)
 	if db.freelist != nil {
 		db.freelist.RemoveReadonlyTXID(tx.meta.Txid())
 	}
 
-	// Unlock the meta pages.
-	db.metalock.Unlock()
-
-	// Merge statistics.
-	db.statlock.Lock()
-	db.stats.OpenTxN = n
-	db.stats.TxStats.add(&tx.stats)
-	db.statlock.Unlock()
+	if db.stats != nil {
+		// Merge statistics.
+		db.statlock.Lock()
+		db.stats.OpenTxN--
+		db.stats.TxStats.add(&tx.stats)
+		db.statlock.Unlock()
+	}
 }
 
 // Update executes a function within the context of a read-write managed transaction.
@@ -1096,9 +1083,13 @@ func (db *DB) Sync() (err error) {
 // Stats retrieves ongoing performance stats for the database.
 // This is only updated when a transaction closes.
 func (db *DB) Stats() Stats {
-	db.statlock.RLock()
-	defer db.statlock.RUnlock()
-	return db.stats
+	var s Stats
+	if db.stats != nil {
+		db.statlock.RLock()
+		s = *db.stats
+		db.statlock.RUnlock()
+	}
+	return s
 }
 
 // This is for internal access to the raw data bytes from the C cursor, use
